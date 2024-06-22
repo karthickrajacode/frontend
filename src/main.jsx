ccc52de358cb1d6367f4f60dfe70ff6a5c4b4f8e
karthick <kart87632@gmail.com>
2024-06-22 11:42:33 +0530

    Alignment formatted for main.jsx file.

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,31 @@
 import React, { createContext, useState } from "react";
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-export const Context = createContext({ isAuthorized: false });
+export const Context = createContext({
+  isAuthorized: false,
+});
 
 const AppWrapper = () => {
   const [isAuthorized, setIsAuthorized] = useState(false);
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
 
   return (
-    <Context.Provider value={{ isAuthorized, setIsAuthorized, user, setUser }}>
+    <Context.Provider
+      value={{
+        isAuthorized,
+        setIsAuthorized,
+        user,
+        setUser,
+      }}
+    >
       <App />
     </Context.Provider>
-  )
-}
+  );
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppWrapper />
   </React.StrictMode>
 );
-
-
