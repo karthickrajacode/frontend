42477107403f37b37d182bb7be551c14e84e1901
karthick <kart87632@gmail.com>
2024-06-22 11:52:51 +0530

    Hostname repaced 127.0.0.1 to localhost in Navbar.jsx file.

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
     const handleLogout = async () => {
         try {
-            const response = await axios.get("http://127.0.0.1:4000/api/v1/user/logout",
+            const response = await axios.get("http://localhost:4000/api/v1/user/logout",
                 {
                     withCredentials: true
                 });
